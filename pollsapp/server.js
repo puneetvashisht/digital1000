c5eab64a471a2b042be88c82eec341595497a3da
puneet <puneetvashsiht@gmail.com>
2016-10-17 11:57:03 +0530

    22

---
+++ pollsapp/server.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json 
+app.use(bodyParser.json())
+
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.post('/polls', function(req,res){
+    
+    // Parse the body and extract JSON
+    console.log(req.body);
+    
+    var obj = {success:true};
+    res.json(obj)
+})
+ 
+app.listen(3000)
